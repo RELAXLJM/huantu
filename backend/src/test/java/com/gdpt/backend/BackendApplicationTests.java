103632e692b065117cafd78aa0dbbf885b25e092
liangjianming <1707622822@qq.com>
2026-07-24 23:10:33 +0800

    初始化项目：创建后端、安卓、前端三个模块

---
+++ backend/src/test/java/com/gdpt/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gdpt.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
